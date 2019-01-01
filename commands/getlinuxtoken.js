c1863b8492b179faac4b1edd68d2df76f566192d
Dutch van der Linde <matthew_ray@corporate.tssservicecollc.com>
2019-01-01 07:08:02 -0500

    allow private message commands

--- commands/getlinuxtoken.js
+++ commands/getlinuxtoken.js
@@ -3,9 +3,8 @@ const config = require('../config.json');
 const client = require('../client');
 
 module.exports = {
-  name: 'gettoken',
+  name: 'getlinuxtoken',
   action: async (msg, args) => {
-    //msg.delete();
     const prevMessage = await msg.channel.createMessage('Generating token...');
     const res = await axios({
       method: 'put',
@@ -15,7 +14,20 @@ module.exports = {
       }
     });
     prevMessage.edit('Your system token has been messaged to you directly, remember not to share this token with anyone else for any reason whatsoever. The only way to regenerate these tokens is by changing your pass.txt file or your username itself.');
+    if (msg.channel.type === 1) {
+        const RichEmbed = require('../RichEmbed');
+        const embed = new RichEmbed();
+        embed.setColor(0x38393E);
+        embed.setTitle('Library of Code | Linux Server API Token');
+        embed.addField('Requester', `${msg.member.user.username}#${msg.member.user.discriminator}`, true);
+        embed.addField('User', args[0], true);
+        embed.addField('Password', args[1], true);
+        embed.addField('Token', res.data, true);
+        embed.setFooter(client.user.username, 'https://images-ext-1.discordapp.net/external/GbYUyna-CcBOWD-zKoLv98EcvLahR_vDm1OzO3m5CoM/%3Fsize%3D128/https/cdn.discordapp.com/avatars/507353658661535754/ee1c00dbd90b5ef6d14589ce10074b30.png');
+        msg.channel.createMessage(embed);
+    } else {
     msg.member.user.getDMChannel().then(c => {
+      msg.delete();
       const RichEmbed = require('../RichEmbed');
       const embed = new RichEmbed();
       embed.setColor(0x38393E);
@@ -27,8 +39,10 @@ module.exports = {
       embed.setFooter(client.user.username, 'https://images-ext-1.discordapp.net/external/GbYUyna-CcBOWD-zKoLv98EcvLahR_vDm1OzO3m5CoM/%3Fsize%3D128/https/cdn.discordapp.com/avatars/507353658661535754/ee1c00dbd90b5ef6d14589ce10074b30.png');
       c.createMessage(embed);
     });
+   }
   },
   options: {
     'description': 'Fetches your token from the API.',
+    'usage': '<username> <password>'
   }
 };   
--- commands/gettoken.js
+++ commands/gettoken.js
@@ -3,9 +3,8 @@ const config = require('../config.json');
 const client = require('../client');
 
 module.exports = {
-  name: 'getlinuxtoken',
+  name: 'gettoken',
   action: async (msg) => {
-    msg.delete();
     const prevMessage = await msg.channel.createMessage('Generating token...');
     const res = await axios({
       method: 'get',
@@ -15,6 +14,17 @@ module.exports = {
       }
     });
     prevMessage.edit('Your token has been messaged to you directly, remember not to share this token with anyone else for any reason whatsoever.');
+    if (msg.channel.type === 1) {
+      const RichEmbed = require('../RichEmbed');
+      const embed = new RichEmbed();
+      embed.setColor(0x38393E);
+      embed.setTitle('Library of Code | API Token');
+      embed.addField('User', `${msg.member.user.username}#${msg.member.user.discriminator}`, true);
+      embed.addField('Token', res.data, true);
+      embed.setFooter(client.user.username, 'https://images-ext-1.discordapp.net/external/GbYUyna-CcBOWD-zKoLv98EcvLahR_vDm1OzO3m5CoM/%3Fsize%3D128/https/cdn.discordapp.com/avatars/507353658661535754/ee1c00dbd90b5ef6d14589ce10074b30.png');
+      msg.channel.createMessage(embed);
+    } else {
+
     msg.member.user.getDMChannel().then(c => {
       const RichEmbed = require('../RichEmbed');
       const embed = new RichEmbed();
@@ -25,9 +35,9 @@ module.exports = {
       embed.setFooter(client.user.username, 'https://images-ext-1.discordapp.net/external/GbYUyna-CcBOWD-zKoLv98EcvLahR_vDm1OzO3m5CoM/%3Fsize%3D128/https/cdn.discordapp.com/avatars/507353658661535754/ee1c00dbd90b5ef6d14589ce10074b30.png');
       c.createMessage(embed);
     });
+  }
   },
   options: {
-    'description': 'Fetches your token from the API.',
-    'usage': '<username> <password>'
+    'description': 'Fetches your token from the API.'
   }
 };   
