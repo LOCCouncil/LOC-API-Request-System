664ef1948a4111125d742762effeaa9cfca9cdc5
Dutch van der Linde <matthew_ray@corporate.tssservicecollc.com>
2019-01-01 06:45:43 -0500

    embeds

--- commands/gettoken.js
+++ commands/gettoken.js
@@ -14,7 +14,16 @@ module.exports = {
       }
     });
     prevMessage.edit('Your token has been messaged to you directly, remember not to share this token with anyone else for any reason whatsoever.');
-    msg.member.user.getDMChannel().then(c => c.createMessage(res.data));
+    msg.member.user.getDMChannel().then(c => {
+      const RichEmbed = require('../RichEmbed');
+      const embed = new RichEmbed();
+      embed.setColor('38393E');
+      embed.setTitle('Library of Code | API Token');
+      embed.addField('User', `${msg.member.user.username}#${msg.member.user.discriminator}`, true);
+      embed.addField('Token', res.data, true);
+      embed.setFooter(client.user.username, 'https://discordapp.com/channels/446067825673633794/468759629334183956');
+      c.createMessage(embed);
+    });
   },
   options: {
     'description': 'Fetches your token from the API.',
